1d3e90a52aede7d9b4f0189497253136266815b4
Ereh-yeager <s.n.bhatti45@gmail.com>
2024-09-16 10:06:28 +0500

    Added Project Folder

---
+++ profile_ui/profile_ui/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ProfileApp());
+
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  final String name = 'Muhammad Abdullah';
+  final String email = 'abdullahwale@gmail.com';
+  final String phone = '+923046983794';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile App'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.amber,
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/79458502?v=4'),
+            ),
+            SizedBox(height: 16),
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              email,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 4),
+            Text(
+              phone,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
