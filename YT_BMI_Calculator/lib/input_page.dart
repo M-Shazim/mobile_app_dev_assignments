6481d40e13c33c56a3f3705c1bd6979c0a12dfa3
Ereh-yeager <s.n.bhatti45@gmail.com>
2024-11-05 10:43:44 +0500

    Fixed inputs

--- YT_BMI_Calculator/lib/input_page.dart
+++ YT_BMI_Calculator/lib/input_page.dart
@@ -17,13 +17,7 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: Row(
                       children: [
-                          Expanded(child: Container(
-                            margin: EdgeInsets.all(15.0),
-                            decoration: BoxDecoration(
-                              color: Color(0xFF1D1E33),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          )),
+                          Expanded(child: RepeatContainerCode()),
                           Expanded(child: Container(
                             margin: EdgeInsets.all(15.0),
                             decoration: BoxDecoration(
@@ -63,4 +57,21 @@ class _InputPageState extends State<InputPage> {
             )
           );
     }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  const RepeatContainerCode({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
 }
