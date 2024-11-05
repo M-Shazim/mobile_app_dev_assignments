b5f4175ad51bc8ba504363ee580470b6085bd43a
Ereh-yeager <s.n.bhatti45@gmail.com>
2024-11-05 17:48:28 +0500

    Added task constants for maintaining constant values across different screens

---
+++ Mobile_dev_Midterm_Lab/midterm_project/lib/repeation/task_constants.dart
@@ -0,0 +1,12 @@
+// task_constants.dart
+
+class TaskConstants {
+  static const String minutely = "minutely";
+  static const String daily = "daily";
+  static const String weekly = "weekly";
+  static const String monthly = "monthly";
+  static const String yearly = "yearly";
+
+  // You can also define a list if you need it for dropdowns or other UI elements
+  static const List<String> intervals = [daily, weekly, monthly, yearly];
+}
