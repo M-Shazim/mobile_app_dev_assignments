3efa2f90c0089b442fb548b1498f452ffe864bfe
Ereh-yeager <s.n.bhatti45@gmail.com>
2024-11-05 21:07:11 +0500

    Modified to include task_revision_frequency

--- Mobile_dev_Midterm_Lab/midterm_project/lib/repeation/task_constants.dart
+++ Mobile_dev_Midterm_Lab/midterm_project/lib/repeation/task_constants.dart
@@ -7,6 +7,8 @@ class TaskConstants {
   static const String monthly = "monthly";
   static const String yearly = "yearly";
 
+  static const Duration checkFrequency = Duration(minutes: 1);
+
   // You can also define a list if you need it for dropdowns or other UI elements
   static const List<String> intervals = [daily, weekly, monthly, yearly];
 }
