544f4ab84ed38dabb5f9a0595c3e80f84c93ea4c
M-Shazim <s.n.bhatti45@gmail.com>
2024-11-16 19:07:31 +0500

    Added icons(male/female) and label @Video 6

--- YT_BMI_Calculator/lib/input_page.dart
+++ YT_BMI_Calculator/lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget{
     @override
@@ -17,8 +18,21 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child: Row(
                       children: [
-                          Expanded(child: RepeatContainerCode(colors : Color(0xFF1D1E33))),
-                          Expanded(child: RepeatContainerCode(colors : Color(0xFF1D1E33))),
+                          Expanded(child: RepeatContainerCode(
+                              colors : Color(0xFF1D1E33),
+                            cardWidget: card_widget(
+                              iconData: Icons.male,
+                              label: "MALE",
+                            ),
+                          ),),
+                          Expanded(child: RepeatContainerCode(
+                            colors : Color(0xFF1D1E33),
+                            cardWidget: card_widget(
+                              iconData: Icons.female,
+                              label: "FEMALE",
+                            ),
+
+                          ),),
                   ],
                 )
                 ),
@@ -35,13 +49,41 @@ class _InputPageState extends State<InputPage> {
     }
 }
 
+class card_widget extends StatelessWidget {
+  card_widget({required this.iconData,required this.label});
+  final IconData iconData;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(label,style: TextStyle(
+          fontSize: 18.0
+        ),)
+      ]
+
+
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-   RepeatContainerCode({required this.colors});
+  RepeatContainerCode({required this.colors, this.cardWidget});
   final Color colors;
+  final Widget? cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
