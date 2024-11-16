60f46763e4ed79403c68d45375dcd52a1c55c745
M-Shazim <s.n.bhatti45@gmail.com>
2024-11-16 16:48:09 +0500

    Did refactoring

--- YT_BMI_Calculator/lib/input_page.dart
+++ YT_BMI_Calculator/lib/input_page.dart
@@ -18,39 +18,15 @@ class _InputPageState extends State<InputPage> {
                     child: Row(
                       children: [
                           Expanded(child: RepeatContainerCode()),
-                          Expanded(child: Container(
-                            margin: EdgeInsets.all(15.0),
-                            decoration: BoxDecoration(
-                              color: Color(0xFF1D1E33),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          )),
+                          Expanded(child: RepeatContainerCode()),
                   ],
                 )
                 ),
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                )),
+                Expanded(child: RepeatContainerCode()),
                 Expanded(child: Row(
                   children: [
-                    Expanded(child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    )),
-                    Expanded(child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),)
+                    Expanded(child: RepeatContainerCode()),
+                    Expanded(child: RepeatContainerCode())
                   ],
                 )),
               ],
