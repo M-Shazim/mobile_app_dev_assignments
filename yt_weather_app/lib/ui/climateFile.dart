eef949c7241f708d79917b5afb586774df5af368
M-Shazim <s.n.bhatti45@gmail.com>
2024-11-18 10:27:29 +0500

    Added image and aligned it @video 3

--- yt_weather_app/lib/ui/climateFile.dart
+++ yt_weather_app/lib/ui/climateFile.dart
@@ -17,11 +17,40 @@ class _ClimateState extends State<Climate> {
         actions: [
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: ()=>,
+            onPressed: ()=>print("Clicked"),
           )
         ],
 
       ),
+      body: Stack(
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage("Images/freezy.jpg"),
+              height: 1200.0,
+              width: 600.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: Text(
+              "Vehari",
+              style: cityStyle(),
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+
+TextStyle cityStyle(){
+  return TextStyle(
+    color: Colors.white,
+    fontSize: 22.9,
+    fontStyle: FontStyle.italic,
+  )
 }
