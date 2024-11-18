5ec01c6c499355d03d72a2171fd36ea7aa7c0a6f
M-Shazim <s.n.bhatti45@gmail.com>
2024-11-18 10:07:45 +0500

    COnfigured the project with separate ui file @video 1

---
+++ yt_weather_app/lib/ui/climateFile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Climate extends StatefulWidget {
+
+  @override
+  State<Climate> createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
